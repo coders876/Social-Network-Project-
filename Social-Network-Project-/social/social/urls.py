43012b85d8b0f2efb12c387af2d9747c87f02756
sjain24 <jain170107062@gmail.com>
2019-01-15 21:19:47 +0530

    minor changes

--- Social-Network-Project-/social/social/urls.py
+++ Social-Network-Project-/social/social/urls.py
@@ -3,7 +3,13 @@ from django.urls import path
 from django.conf.urls import url,include
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.auth import views as auth_views
+
+
+
 urlpatterns = [
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     path('admin/', admin.site.urls),
     path('login/', include('login.urls'))
 ]
